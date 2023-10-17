24af608f04e8175a5a326607c360273e46d828f3
Oscar Encarnacion <oscar0alejandro@gmail.com>
2023-10-17 02:39:48 -0600

    Primera carga de archivos

---
+++ CurveFitting/Clases/Generador.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace CurveFitting.Clases
+{
+    internal class Generador
+    {
+        public List<(float, float)>? ListDataSet { get; set; }
+        public float Beta0 { get; set; }
+        public float Beta1 { get; set; }
+        public string Ecuacion { get; set; }
+        public Generador(List<(float, float)> dataSet)
+        {
+            if (dataSet.Count > 0)
+            {
+                ListDataSet = dataSet;
+                CalcularBetas();
+            }
+            else
+            {
+                MessageBox.Show("El dataset esta vacio.");
+            }
+        }
+
+        private void CalcularBetas()
+        {
+            float count = ((float)ListDataSet.Count);
+            float sumX = 0f;
+            float sumY = 0f;
+            float sumXY = 0f;
+            float sumXX = 0f;
+            foreach ((float advertising, float sales) in ListDataSet)
+            {
+                sumX += advertising;
+                sumY += sales;
+                sumXY += advertising * sales;
+                sumXX += ((float)Math.Pow(advertising, 2));
+            }
+            Beta1 = (count * sumXY - sumX * sumY) / (count * sumXX - ((float)Math.Pow(sumX, 2)));
+            Beta0 = (sumY - Beta1 * sumX) / count;
+            Ecuacion = $"y = {Beta0} + {Beta1}x";
+        }
+
+        public float PredictSales(float advertising)
+        {
+            if (advertising > 0)
+            {
+                return Beta0 + Beta1 * advertising;
+            }
+            else
+            {
+                return -1f;
+            }
+        }
+    }
+}
---
+++ CurveFitting/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using CurveFitting.Clases;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CurveFitting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Actualizar este data set si se desea analizar otro
+        private static readonly List<(float, float)> dataSet = new()
+        {
+            (23f, 651f),
+            (26f, 762f),
+            (30f, 856f),
+            (34f, 1063f),
+            (43f, 1190f),
+            (48f, 1298f),
+            (52f, 1421f),
+            (57f, 1440f),
+            (58f, 1518f)
+        };
+        private Generador generador = new(dataSet);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LlenarControles();
+        }
+
+        private void LlenarControles()
+        {
+            ecuacionTextBox.Text = generador.Ecuacion;
+            dataSetView.Text = "\nAdvertising\tSales\n";
+            foreach ((float advertising, float sales) in dataSet)
+            {
+                dataSetView.Text += $"{advertising}\t\t{sales}\n";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (float.TryParse(advertisingTextBox.Text, out float advertising))
+            {
+                float sales = generador.PredictSales(advertising);
+                salesTextBox.Text = sales.ToString();
+            }
+            else
+            {
+                MessageBox.Show("El valor ingresado no es valido.");
+            }
+        }
+    }
+}
